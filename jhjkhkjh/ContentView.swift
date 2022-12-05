eb88e3d15ae4414757e090e31036aa2d45557042
Zara <zhaider@students.hackleyschool.org>
2022-12-05 09:02:22 -0500

    Initial Commit

---
+++ jhjkhkjh/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  jhjkhkjh
+//
+//  Created by Zara Haider on 12/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ jhjkhkjh/jhjkhkjhApp.swift
@@ -0,0 +1,17 @@
+//
+//  jhjkhkjhApp.swift
+//  jhjkhkjh
+//
+//  Created by Zara Haider on 12/5/22.
+//
+
+import SwiftUI
+
+@main
+struct jhjkhkjhApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
